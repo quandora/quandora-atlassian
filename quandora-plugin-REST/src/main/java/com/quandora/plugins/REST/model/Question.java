d2f60504ea6ea6187f494a2d012a14889f53ab39
Nicolas Joseph <nicolas@quandora.com>
2013-06-25 11:52:41 -0400

    JIRA - skin Change - REST - Fix Xml encoding issue

--- quandora-plugin-REST/src/main/java/com/quandora/plugins/REST/model/Question.java
+++ quandora-plugin-REST/src/main/java/com/quandora/plugins/REST/model/Question.java
@@ -1,6 +1,10 @@
 package com.quandora.plugins.REST.model;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
@@ -73,7 +77,7 @@ public class Question {
      * @param title the title to set
      */
     public void setTitle(String title) {
-        this.title = title;
+        this.title =  unescapeXML(title);
     }
 
 
@@ -93,7 +97,7 @@ public class Question {
      * @param summary the summary to set
      */
     public void setSummary(String summary) {
-        this.summary = summary;
+        this.summary = unescapeXML(summary);
     }
 
 
@@ -222,5 +226,52 @@ public class Question {
     @Override
     public String toString() {
         return title;
+    }
+
+    private static String unescapeXML( final String xml )
+    {
+        Pattern xmlEntityRegex = Pattern.compile( "&(#?)([^;]+);" );
+        //Unfortunately, Matcher requires a StringBuffer instead of a StringBuilder
+        StringBuffer unescapedOutput = new StringBuffer( xml.length() );
+
+        Matcher m = xmlEntityRegex.matcher( xml );
+        Map<String,String> builtinEntities = null;
+        String entity;
+        String hashmark;
+        String ent;
+        int code;
+        while ( m.find() ) {
+            ent = m.group(2);
+            hashmark = m.group(1);
+            if ( (hashmark != null) && (hashmark.length() > 0) ) {
+                code = Integer.parseInt( ent );
+                entity = Character.toString( (char) code );
+            } else {
+                //must be a non-numerical entity
+                if ( builtinEntities == null ) {
+                    builtinEntities = buildBuiltinXMLEntityMap();
+                }
+                entity = builtinEntities.get( ent );
+                if ( entity == null ) {
+                    //not a known entity - ignore it
+                    entity = "&" + ent + ';';
+                }
+            }
+            m.appendReplacement( unescapedOutput, entity );
+        }
+        m.appendTail( unescapedOutput );
+
+        return unescapedOutput.toString();
+    }
+
+    private static Map<String,String> buildBuiltinXMLEntityMap()
+    {
+        Map<String,String> entities = new HashMap<String,String>(10);
+        entities.put( "lt", "<" );
+        entities.put( "gt", ">" );
+        entities.put( "amp", "&" );
+        entities.put( "apos", "'" );
+        entities.put( "quot", "\"" );
+        return entities;
     }
 }
